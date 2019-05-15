ab3c9653ddf805dee8423b64e1e6f619d3c4163e
mohsen <mim_z@hotmail.com>
2019-05-15 17:36:23 +0430

    add requirement instance variable and create a constructor

--- ChatClient/src/ir/maktabsharif/ChatClient.java
+++ ChatClient/src/ir/maktabsharif/ChatClient.java
@@ -1,4 +1,26 @@
 package ir.maktabsharif;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class ChatClient {
+
+    private final String serverName;
+    private final int serverPort;
+    private Socket socket;
+    private InputStream serverIn;
+    private OutputStream serverOut;
+    private BufferedReader bufferedIn;
+
+    private ArrayList<UserStatusListener> userStatusListeners = new ArrayList<>();
+    private ArrayList<MessageListener> messageListeners = new ArrayList<>();
+
+    //constructor
+    public ChatClient(String serverName, int serverPort) {
+        this.serverName = serverName;
+        this.serverPort = serverPort;
+    }
 }
