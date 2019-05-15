43585a8f2244249047d4d8bfaf99bde095d28f45
Mohammad747 <mohammad.hashemi747@gamil.com>
2019-05-15 15:23:58 +0430

    Server
    Implement the sever logic and now it can accept the clients and show the
    port of the client witch is now connected.

--- ChatServer/src/ir/maktab/ServerMain.java
+++ ChatServer/src/ir/maktab/ServerMain.java
@@ -1,4 +1,25 @@
 package ir.maktab;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerMain {
+    public static void main(String[] args) {
+        int port = 8818;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                System.out.println();
+                System.out.println("About to accept client connection...");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Accepted connection from " + clientSocket);
+                OutputStream outputStream = clientSocket.getOutputStream();
+                outputStream.write("Hello World!".getBytes());
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
