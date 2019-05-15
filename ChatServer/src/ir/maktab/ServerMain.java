056129d6d60aabb886e54f439ab7b42c0bf2aaf4
Mohammad747 <mohammad.hashemi747@gamil.com>
2019-05-15 18:47:34 +0430

    Server class
    Add server class to store a list of online clients

--- ChatServer/src/ir/maktab/ServerMain.java
+++ ChatServer/src/ir/maktab/ServerMain.java
@@ -1,32 +1,13 @@
 package ir.maktab;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-/**
- * author mohammad hashemi
- *
- */
-
 public class ServerMain {
-    public static void main(String[] args) {
-        //Port of the server that each client should connect to.
-        int port = 8818;
-        //Use the main thread to accept connection from clients
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            while (true) {
-                System.out.println();
-                System.out.println("About to accept client connection...");
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Accepted connection from " + clientSocket);
-                //Use another thread to handle the communication with the established client's connection.
-                ServerWorker worker = new ServerWorker(clientSocket);
-                worker.start();
 
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) {
+
+        int port = 8818;
+        Server server = new Server(port);
+        server.start();
     }
+
+
 }
