464410645f721c32120a2db97fb7d64adf662042
mohsen <mim_z@hotmail.com>
2019-05-15 17:40:26 +0430

    add methods for send massage and login

--- ChatClient/src/ir/maktabsharif/ChatClient.java
+++ ChatClient/src/ir/maktabsharif/ChatClient.java
@@ -1,6 +1,7 @@
 package ir.maktabsharif;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -23,4 +24,26 @@ public class ChatClient {
         this.serverName = serverName;
         this.serverPort = serverPort;
     }
+
+    public void msg(String sendTo, String msgBody) throws IOException {
+        String cmd = "msg " + sendTo + " " + msgBody + "\n";
+        serverOut.write(cmd.getBytes());
+    }
+
+    public boolean login(String login, String password) throws IOException {
+        String cmd = "login " + login + " " + password + "\n";
+        serverOut.write(cmd.getBytes());
+
+        String response = bufferedIn.readLine();
+        System.out.println("Response Line:" + response);
+
+        if ("ok login".equalsIgnoreCase(response)) {
+            startMessageReader();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    
 }
